f909cb61420d58c550a2ba9cf4f025ef545b5323
Orlando de Mauro <odemauro@everis.com>
2018-02-21 16:38:26 +0100

    added comments

--- src/menu/scripts_list_creator.js
+++ src/menu/scripts_list_creator.js
@@ -11,13 +11,16 @@ let result;
 //get the list of files and order it
 if (fs.existsSync(folder)) {
     var files = fs.readdirSync(folder).filter(function(file) {
+        //rename the loader.bundle removing the hash from the name
         if (/^loader.*\.js$/.test(file)) {
             fs.renameSync(path.resolve(__dirname, 'dist', file), path.resolve(__dirname, 'dist/loader.bundle.js'));
             return false;
         }
+        //return only the .js and .css files
         return /\.(js|css)$/.test(path.extname(file).toLowerCase());
     });
     
+    //sort the array,the angular-cli bundles needs to be loaded following a specific order
     result = regexSort(files, [
         /^inline.*\.js$/,
         /^(?!(inline|main|loader)).*\.(js|css)$/
